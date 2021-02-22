1de49c76ce2fde2777c5c4289818832e30ec2e2b
AminKhaled <aminabdo8@gmail.com>
2021-02-22 21:54:27 +0200

    we added about-us page with reposive design

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(window).scroll(function (){
+        var scroll = $(window).scrollTop();
+        if(scroll > 100){
+            $('#nav').css('background','#fff')
+        }else {
+            $('#nav').css('background','rgba(255,255,255,0.6)')
+        }
+    })
+})

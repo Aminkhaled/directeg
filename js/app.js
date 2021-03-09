d7e384cdcdbd6c14a6e017bf1337fb7a140e645f
AminKhaled <aminabdo8@gmail.com>
2021-03-09 16:39:49 +0200

    we added menu to be like mobile

--- js/app.js
+++ js/app.js
@@ -3,14 +3,16 @@ $(document).ready(function () {
         var scroll = $(window).scrollTop();
         if(scroll > 100){
             $('#nav').css('background','#fff')
+            $('#nav-mobile').css('background','#fff')
         }else {
             $('#nav').css('background','rgba(255,255,255,0.6)')
+            $('#nav-mobile').css('background','rgba(255,255,255,0.6)')
+
         }
     })
-    new WOW().init();
 
     $('.icon-span').click(function (e){
         e.defaultPrevented;
-        $('.nav-bottom').toggleClass('active');
+        $('.nav-bottom-mobile').toggleClass('activee');
     });
 })
